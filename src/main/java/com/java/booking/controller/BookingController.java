03b52173984599191b987f9c9802a8d4009e1e49
Namrata_Singh <nmrtsingh79@gmail.com>
2026-09-01 11:40:06 +0530

    MCH-001-Implement RestTemplate

--- src/main/java/com/java/booking/controller/BookingController.java
+++ src/main/java/com/java/booking/controller/BookingController.java
@@ -1,6 +1,8 @@
 package com.java.booking.controller;
 
 import com.java.booking.entity.Booking;
+import com.java.booking.model.BookingUserDto;
+import com.java.booking.model.UserDto;
 import com.java.booking.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,4 +25,14 @@ public class BookingController {
     public List<Booking> getAllBookings() {
         return bookingService.getAllBookings();
     }
+
+    @GetMapping("/{bookingId}")
+    public BookingUserDto getBookingById(@PathVariable Long bookingId) {
+        return bookingService.getBookingById(bookingId);
+    }
+
+    /*@GetMapping("/user/{userId}")
+    public UserDto getUser(@PathVariable Long userId) {
+        return bookingService.getUser(userId);
+    }*/
 }
---
+++ src/main/java/com/java/booking/model/UserDto.java
@@ -0,0 +1,8 @@
+package com.java.booking.model;
+
+public record UserDto (
+        Long id,
+        String firstName,
+        String lastName,
+        String email
+) {}
